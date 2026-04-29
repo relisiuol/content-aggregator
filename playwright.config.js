aea82014b07d01a78f8cde60dc2ed4568ca97c18
Louis <contact@relisiuol.fr>
2026-04-30 01:25:26 +0200

    Tests: align Playwright with wp-env dev port

--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,14 @@ const defaultConfig = require( '@wordpress/scripts/config/playwright.config.js' 
 
 export default {
 	...defaultConfig,
+	use: {
+		...defaultConfig.use,
+		baseURL: 'http://localhost:8888/',
+	},
 	webServer: {
 		...defaultConfig.webServer,
 		command: 'pnpm run build && pnpm run wp-env start',
+		port: 8888,
 		timeout: 240_000,
 	},
 };
